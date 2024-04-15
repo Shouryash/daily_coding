230520236d087d449ca8ab4ec88f8a2b86556c3c
Shouryash <129163161+Shouryash@users.noreply.github.com>
2024-04-15 14:23:37 +0530

    Add files via upload

---
+++ Area_of_circle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Area_of_circle {
+	public static void main(String args[]) {
+		
+		double PI=3.14;
+		double areaOfCircle;
+		double radius;
+		double circumference;
+		Scanner Sc=new Scanner(System.in);
+				
+		System.out.print("Enter the Radius:-");
+		radius=Sc.nextDouble();
+		
+		areaOfCircle= PI * radius * radius;
+		circumference= 2 * PI * radius;
+		
+		System.out.println("Area Of Circle is: % .2f" +areaOfCircle);
+		
+		System.out.println("circumference of circle  is : % .2f" +areaOfCircle);
+			}
+	
+}
