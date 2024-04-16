fc2d898a9e1b8686c0138fb899ed80600f8cbefc
Shouryash <129163161+Shouryash@users.noreply.github.com>
2024-04-16 19:18:31 +0530

    Add files via upload

---
+++ Sweetch.java
@@ -0,0 +1,16 @@
+//switch statement simple program
+public class Sweetch {
+    public static void main(String[] args) {
+        int isNUmber =10;
+        switch (isNUmber) {
+            case 10:
+                System.out.println("Hellow");
+                break;
+            case 20:
+                System.out.println("BA PASS");
+        
+            default:
+                System.out.println("None");
+        }
+    }
+}
